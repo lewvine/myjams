91131f6dbe9a7d96661d0f9273eb90048a143018
Lewis Vine <lew.vine@gmail.com>
2021-05-29 13:57:16 -0400

    Refactored additional features with jQuery

---
+++ scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Blue', duration: '4:26' },
+        { title: 'Green', duration: '3:14' },
+        { title: 'Red', duration: '5:01' },
+        { title: 'Pink', duration: '3:21' },
+        { title: 'Magenta', duration: '2:15' }
+    ]
+};
+
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Hello, Operator?', duration: '1:01' },
+        { title: 'Ring, ring, ring', duration: '5:01' },
+        { title: 'Fits in your pocket', duration: '3:21' },
+        { title: 'Can you here me now?', duration: '3:14' },
+        { title: 'Wrong phone number', duration: '2:15' }
+    ]
+};
+
+var albumHannah = {
+    title: 'Hannahs songs',
+    artist: 'Hannah Grace Vine',
+    label: 'Kids songs',
+    year: '2021',
+    albumArtUrl: 'assets/images/album_covers/19.png',
+    songs: [
+        { title: 'Its a stunk!', duration: '1:01' },
+        { title: 'I love you daddy!', duration: '5:01' },
+        { title: 'Time for swim class', duration: '3:21' },
+        { title: 'I dont want to go to bed', duration: '3:14' },
+        { title: 'Will you read me a book?', duration: '2:15' }
+    ]
+};
